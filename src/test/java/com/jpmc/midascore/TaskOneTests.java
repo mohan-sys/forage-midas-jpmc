a20402e0b07af05e6a512123a987f608c0ad034a
Mohan Raj <mohanraj123.mrl@gmail.com>
2025-08-14 12:53:29 +0100

    step 1: dependency

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -5,7 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "general.kafka-topic=test-topic"
+})
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
 
